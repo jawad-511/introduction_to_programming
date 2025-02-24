d5f414b1674189be61401f9bec1ac914517115c2
Jawad Alotaibi <120018948+jawad-511@users.noreply.github.com>
2025-02-24 16:16:18 +0300

    UserInputVideo.cpp

--- User_Input/UserInputVideo.cpp
+++ User_Input/UserInputVideo.cpp
@@ -15,57 +15,57 @@ int main()
     // cout << "You enterd this char: " << myChar << " and this number: " << myNumber;
 
     // homework 1
-    // string name;
-    // int age;
-    // string city;
-    // string country;
-    // float monthlySalary;
-    // char gender;
-    // bool isMarried;
+    string name;
+    int age;
+    string city;
+    string country;
+    float monthlySalary;
+    char gender;
+    bool isMarried;
 
-    // cout << "Please enter your name: \n";
-    // cin >> name;
+    cout << "Please enter your name: \n";
+    cin >> name;
 
-    // cout << "Please enter your age: \n";
-    // cin >> age;
+    cout << "Please enter your age: \n";
+    cin >> age;
 
-    // cout << "Please enter your country: \n";
-    // cin >> country;
+    cout << "Please enter your country: \n";
+    cin >> country;
 
-    // cout << "Please enter your montly salary: \n";
-    // cin >> monthlySalary;
+    cout << "Please enter your montly salary: \n";
+    cin >> monthlySalary;
 
-    // cout << "Please enter the first letter of your gender: \n";
-    // cin >> gender;
+    cout << "Please enter the first letter of your gender: \n";
+    cin >> gender;
 
-    // cout << "Please answer the question are you married? answer with true/false \n";
-    // cin >> isMarried;
+    cout << "Please answer the question are you married? answer with true/false \n";
+    cin >> isMarried;
 
-    // cout << "Name: " << name << endl;
-    // cout << "Age: " << age << " years" << endl;
-    // cout << "City: " << city << endl;
-    // cout << "country: " << country << '.' << endl;
-    // cout << "Monthly Salary: " << monthlySalary << endl;
-    // cout << "Yearly Salary: " << monthlySalary * 12 << endl;
-    // cout << "Gender : " << gender << endl;
-    // cout << "Married ?: " << isMarried << endl;
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << " years" << endl;
+    cout << "City: " << city << endl;
+    cout << "country: " << country << '.' << endl;
+    cout << "Monthly Salary: " << monthlySalary << endl;
+    cout << "Yearly Salary: " << monthlySalary * 12 << endl;
+    cout << "Gender : " << gender << endl;
+    cout << "Married ?: " << isMarried << endl;
 
     //homework2
-    // int num1 , num2 , num3 , sum;
-    // cout << "Enter The first number: \n";
-    // cin >> num1;
+    int num1 , num2 , num3 , sum;
+    cout << "Enter The first number: \n";
+    cin >> num1;
 
-    // cout << "Enter The second number: \n";
-    // cin >> num2;
+    cout << "Enter The second number: \n";
+    cin >> num2;
     
-    // cout << "Enter The third number: \n";
-    // cin >> num3;
+    cout << "Enter The third number: \n";
+    cin >> num3;
 
-    // cout << "\n";
-    // sum = num1  + num2 + num3;
+    cout << "\n";
+    sum = num1  + num2 + num3;
 
 
-    // cout << num1 <<" +\n" << num2 <<" +\n" << num3 << "\n" << "---------------------------------------\n" << "total = " << num1 + num2 + num3;
+    cout << num1 <<" +\n" << num2 <<" +\n" << num3 << "\n" << "---------------------------------------\n" << "total = " << num1 + num2 + num3;
 
     //homework3
     int age;
